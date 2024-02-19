95513243c4de8d6fd2885c7d88a79665ba56d7ae
user <omri.kochavi@asperii.com>
2024-02-19 15:19:19 +0000

    fix wording

--- app/contact/page.js
+++ app/contact/page.js
@@ -7,7 +7,7 @@ const ContactPage = () => {
   return (
     <div className="flex flex-col p-6">
       <h1 className="text-2xl font-bold mb-4">Contact</h1>
-      <p className="mb-4">For all enquiries, email me at at <Link className='underline' href={`mailto:${EMAIL_ADDRESS}`}>{EMAIL_ADDRESS}</Link>.</p>
+      <p className="mb-4">For all enquiries, email me at <Link className='underline' href={`mailto:${EMAIL_ADDRESS}`}>{EMAIL_ADDRESS}</Link>.</p>
     </div>
   );
 };
